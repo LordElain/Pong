dd4422aa949dc197b4b9f536e4f96b0079606939
Kevin "Elain" K <47543964+LordElain@users.noreply.github.com>
2023-05-21 19:31:21 +0200

    add: Ball Physics

---
+++ Assets/SCRIPTS/Ball.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float ballSpeed;
+    [SerializeField] private int chosenSide;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void chooseDirection()
+    {
+        chosenSide = Random.Range(0, 2);
+        //0 - Left, 1 - Right
+    }
+
+    public void Reset()
+    {
+        transform.position = Vector2.zero;
+    }
+
+    public void GameStart()
+    {
+        //rb.MovePosition((Vector2)transform.position + (new Vector2(0, y) * (playerSpeed * Time.deltaTime)));
+
+        if (chosenSide == 1)
+        {
+            rb.velocity = Vector2.right * ballSpeed;
+        }
+        else
+        {
+            rb.velocity = Vector2.left * ballSpeed;
+        }
+    }
+
+    public void Movement()
+    {
+        rb.velocity = new Vector2(-1, -1) * ballSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("TRIGGERED");
+        if (col.gameObject.tag == "WALL" || col.gameObject.tag == "PLAYER")
+        {
+            Movement();
+        }
+    }
+}
--- Assets/SCRIPTS/MANAGER/GM.cs
+++ Assets/SCRIPTS/MANAGER/GM.cs
@@ -8,17 +8,27 @@ public class GM : MonoBehaviour
     #region GameRunning
 
     [SerializeField] private bool GameRunning;
+    [SerializeField] private Ball ball;
 
     #endregion
     // Start is called before the first frame update
     void Start()
     {
         GameRunning = true;
+        ball.chooseDirection();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameRunning)
+        {
+            ball.GameStart();
+        }
+    }
+
+    void Goal()
+    {
+        ball.Reset();
     }
 }
